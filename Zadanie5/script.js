4f27f317ccaa93b420a88b07dd2bc0f538e94b1f
ptkk73 <ptkk73@gmail.com>
2015-11-25 20:01:43 +0100

    Coords & box begin

--- Zadanie5/script.js
+++ Zadanie5/script.js
@@ -8,19 +8,23 @@ var VSHADER_SOURCE =
     'uniform mat4 matrixProj; \n' +
     'uniform mat4 matrixMv; \n' +
     'attribute vec3 vPos; \n' +
-    'attribute vec3 vCol; \n' +
-    //'varying vec3 varPos; \n' +
+    'varying vec3 varCol; \n' +
     'void main() { \n' +
-        //'varPos = vPos; \n'
-    ' gl_Position = matrixProj * matrixMv * vec4( vPos.xyz, 1.0); } \n'; //+
+
+    ' gl_Position = matrixProj * matrixMv * vec4( vPos.xyz, 1.0); \r\n' +
+    'varCol = vPos; \n' +
+    '} \n'; //+
     //'} \n';
 
 var FSHADER_SOURCE =
+
     'precision mediump float; \n' +
     'uniform float time; \n' +
-    //'varying vec3 varPos; \n' +
+    'uniform vec3 col; \n' +
+    'uniform vec2 mPos; \n' +
+    'varying vec3 varCol; \n' +
     'void main() { \n' +
-    ' gl_FragColor = vec4(1.0, sin(time * 0.001) * 0.5 + 0.5, abs(sin(time * 0.002)), 1.0); \n' +
+    ' gl_FragColor = vec4(col, 1) * vec4(col, 1.0); \n' +
     '} \n';
 
 var cursorX = 0.0;
@@ -35,6 +39,7 @@ var uniform_mpos;
 var uniform_matrixProj;
 var uniform_matrixView;
 var attribute_vPos;
+var colorLocation;
 var gl;
 
 var modelViewPersp;
@@ -44,6 +49,8 @@ var mvMatrix;
 
 var vBuffer;
 var vBufferTriangle;
+var vBufferPyramid;
+var vBufferBox;
 var vBufferTriangleLocation;
 
 var gridBuffer;
@@ -58,7 +65,8 @@ var pointVerts =  [
 
 var pacmanVerts =  [];
 var triangleVerts =  [];
-
+var pyramidVerts =  [];
+var boxVerts =  [];
 
 function toRad(d)
 {
@@ -95,7 +103,7 @@ function buildGridBuffer()
     gridBuffer.numItems = 6;
 }
 
-function drawLine(x1, y1, z1, x2, y2, z2)
+function drawLine(x1, y1, z1, x2, y2, z2, r, g, b)
 {
     var lineBufV = []
     lineBufV.push(x1, y1, z1, x2, y2, z2);
@@ -104,23 +112,7 @@ function drawLine(x1, y1, z1, x2, y2, z2)
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(lineBufV), gl.DYNAMIC_DRAW);
     vBuffer.itemSize = 3;
     vBuffer.numItems = 2;
-
-    gl.enableVertexAttribArray(attribute_vPos);
-    gl.vertexAttribPointer(attribute_vPos, 3, gl.FLOAT, false, 0, 0);
-    gl.drawArrays(gl.LINES, 0, 2);
-    gl.disableVertexAttribArray(attribute_vPos);
-}
-
-function drawLine(x1, y1, z1, x2, y2, z2, r, g, b, a)
-{
-    var lineBufV = []
-    lineBufV.push(x1, y1, z1, x2, y2, z2);
-    var lineBuf = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, lineBuf);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(lineBufV), gl.DYNAMIC_DRAW);
-    vBuffer.itemSize = 3;
-    vBuffer.numItems = 2;
-
+    gl.uniform3f(colorLocation, r, g, b);
     gl.enableVertexAttribArray(attribute_vPos);
     gl.vertexAttribPointer(attribute_vPos, 3, gl.FLOAT, false, 0, 0);
     gl.drawArrays(gl.LINES, 0, 2);
@@ -155,9 +147,9 @@ function buildPacmanBuffer(time) {
 function buildTriangleBuffer(time) {
 
     triangleVerts = [];
-    triangleVerts.push(0, 10, 0);
-    triangleVerts.push(5, -10, 0);
-    triangleVerts.push(-5, -10, 0);
+    triangleVerts.push(0, 1, 0);
+    triangleVerts.push(0.5, -1, 0);
+    triangleVerts.push(-0.5, -1, 0);
 
 
 
@@ -165,6 +157,61 @@ function buildTriangleBuffer(time) {
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(triangleVerts), gl.DYNAMIC_DRAW);
     vBufferTriangle.itemSize = 3;
     vBufferTriangle.numItems = 3;
+}
+
+function buildPyramidBuffer(time) {
+
+    pyramidVerts = [];
+    pyramidVerts.push(0, 1, 0);
+    pyramidVerts.push(-1, -1, 0);
+    pyramidVerts.push(1, -1, 0);
+
+    pyramidVerts.push(0, 1, 0);
+    pyramidVerts.push(-1, -1, 0);
+    pyramidVerts.push(0, 0, 3);
+
+    pyramidVerts.push(0, 1, 0);
+    pyramidVerts.push(0, 0, 3);
+    pyramidVerts.push(1, -1, 0);
+
+    pyramidVerts.push(0, 0, 3);
+    pyramidVerts.push(-1, -1, 0);
+    pyramidVerts.push(1, -1, 0);
+
+
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, vBufferPyramid);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(pyramidVerts), gl.DYNAMIC_DRAW);
+    vBufferPyramid.itemSize = 3;
+    vBufferPyramid.numItems = 3 * 4;
+}
+
+function buildBox() {
+
+    boxVerts = [];
+    boxVerts.push(-1, -1, 1);
+    boxVerts.push(-1, 1, 1);
+    boxVerts.push(1, 1, 1);
+
+
+    boxVerts.push(1, 1, 1);
+    boxVerts.push(1, -1, 1);
+    boxVerts.push(-1, -1, 1);
+
+    boxVerts.push(-1, -1, -1);
+    boxVerts.push(-1, 1, -1);
+    boxVerts.push(1, 1, -1);
+
+
+    boxVerts.push(1, 1, -1);
+    boxVerts.push(1, -1, -1);
+    boxVerts.push(-1, -1, -1);
+
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, vBufferBox);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(boxVerts), gl.DYNAMIC_DRAW);
+    vBufferBox.itemSize = 3;
+    vBufferBox.numItems = 3 * 4 ;
 }
 
 function drawGrid()
@@ -182,8 +229,9 @@ function drawTriangle()
     mat4.identity(mvMatrix);
     //mat4.translate(mvMatrix, [time * 0.0001 * 20, time * 0.0001 * 60, 0.0]);
     mat4.scale(mvMatrix, [1, 1, 1]);
-    mat4.rotate(mvMatrix, time * 0.001, [1.0, 0.0, 1.0 ]);
-
+    mat4.translate(mvMatrix, [Math.sin(time * 0.01) * 1, Math.cos(time * 0.01) * 1, -1.0 ]);
+    //mat4.rotate(mvMatrix, time * 0.001, [1.0, 0.0, 1.0 ]);
+    gl.uniform3f(colorLocation, 1, 1, 1);
     gl.uniformMatrix4fv(uniform_matrixView, false, mvMatrix);
 
     gl.bindBuffer(gl.ARRAY_BUFFER, vBufferTriangle);
@@ -191,6 +239,78 @@ function drawTriangle()
     gl.enableVertexAttribArray(attribute_vPos);
     gl.vertexAttribPointer(attribute_vPos, 3, gl.FLOAT, false, 0, 0);
     gl.drawArrays(gl.TRIANGLES, 0, vBufferTriangle.numItems);
+    gl.disableVertexAttribArray(attribute_vPos);
+}
+
+function drawPyramidUp()
+{
+    mat4.identity(mvMatrix);
+    //mat4.translate(mvMatrix, [time * 0.0001 * 20, time * 0.0001 * 60, 0.0]);
+    mat4.scale(mvMatrix, [1, 1, 1]);
+    mat4.translate(mvMatrix, [0, 7, 0.0 ]);
+    mat4.rotate(mvMatrix, -Math.PI * 0.5, [1.0, 0.0, 0.0 ]);
+    gl.uniform3f(colorLocation, 1, 0, 0);
+    gl.uniformMatrix4fv(uniform_matrixView, false, mvMatrix);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, vBufferPyramid);
+
+    gl.enableVertexAttribArray(attribute_vPos);
+    gl.vertexAttribPointer(attribute_vPos, 3, gl.FLOAT, false, 0, 0);
+    gl.drawArrays(gl.TRIANGLES, 0, vBufferPyramid.numItems);
+    gl.disableVertexAttribArray(attribute_vPos);
+}
+
+function drawPyramidRight()
+{
+    mat4.identity(mvMatrix);
+    //mat4.translate(mvMatrix, [time * 0.0001 * 20, time * 0.0001 * 60, 0.0]);
+    mat4.scale(mvMatrix, [1, 1, 1]);
+    mat4.translate(mvMatrix, [7, 0, 0.0 ]);
+    mat4.rotate(mvMatrix, Math.PI * 0.5, [0.0, 1.0, 0.0 ]);
+    gl.uniform3f(colorLocation, 0, 1, 0);
+    gl.uniformMatrix4fv(uniform_matrixView, false, mvMatrix);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, vBufferPyramid);
+
+    gl.enableVertexAttribArray(attribute_vPos);
+    gl.vertexAttribPointer(attribute_vPos, 3, gl.FLOAT, false, 0, 0);
+    gl.drawArrays(gl.TRIANGLES, 0, vBufferPyramid.numItems);
+    gl.disableVertexAttribArray(attribute_vPos);
+}
+
+function drawBox()
+{
+    mat4.identity(mvMatrix);
+    //mat4.translate(mvMatrix, [time * 0.0001 * 20, time * 0.0001 * 60, 0.0]);
+    mat4.scale(mvMatrix, [1, 1, 1]);
+    //mat4.translate(mvMatrix, [7, 0, 0.0 ]);
+    //mat4.rotate(mvMatrix, Math.PI * 0.5, [0.0, 1.0, 0.0 ]);
+    gl.uniform3f(colorLocation, 0.6, 0.4, 0.8);
+    gl.uniformMatrix4fv(uniform_matrixView, false, mvMatrix);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, vBufferBox);
+
+    gl.enableVertexAttribArray(attribute_vPos);
+    gl.vertexAttribPointer(attribute_vPos, 3, gl.FLOAT, false, 0, 0);
+    gl.drawArrays(gl.TRIANGLES, 0, vBufferBox.numItems);
+    gl.disableVertexAttribArray(attribute_vPos);
+}
+
+function drawPyramidFar()
+{
+    mat4.identity(mvMatrix);
+    //mat4.translate(mvMatrix, [time * 0.0001 * 20, time * 0.0001 * 60, 0.0]);
+    mat4.scale(mvMatrix, [1, 1, 1]);
+    mat4.translate(mvMatrix, [0, 0, 7.0 ]);
+    mat4.rotate(mvMatrix, Math.PI * 0.5, [0.0, 0.0, 1.0 ]);
+    gl.uniform3f(colorLocation, 0, 0, 1);
+    gl.uniformMatrix4fv(uniform_matrixView, false, mvMatrix);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, vBufferPyramid);
+
+    gl.enableVertexAttribArray(attribute_vPos);
+    gl.vertexAttribPointer(attribute_vPos, 3, gl.FLOAT, false, 0, 0);
+    gl.drawArrays(gl.TRIANGLES, 0, vBufferPyramid.numItems);
     gl.disableVertexAttribArray(attribute_vPos);
 }
 
@@ -205,15 +325,20 @@ function draw()
     //gl.drawArrays(gl.TRIANGLE_FAN, 0, vBuffer.numItems);
     //gl.disableVertexAttribArray(attribute_vPos);
 
-    drawTriangle();
 
     mat4.identity(mvMatrix);
     gl.uniformMatrix4fv(uniform_matrixView, false, mvMatrix);
 
-    drawLine(0, -10, 0, 0, 10, 0);
-    drawLine(-10, 0, 0, 10, 0, 0);
-    drawLine(0, 0, 10, 0, 0, -10);
-    //drawLine(-2, 3, 0, 0);
+    drawLine(0, -10, 0, 0, 10, 0    , 1, 0, 0);
+    drawLine(-10, 0, 0, 10, 0, 0    , 0, 1, 0);
+    drawLine(0, 0, 10, 0, 0, -10    , 0, 0, 1);
+
+    drawTriangle();
+    drawPyramidUp();
+    drawPyramidRight();
+    drawPyramidFar();
+
+    drawBox();
 }
 
 function update()
@@ -222,18 +347,20 @@ function update()
 
     buildPacmanBuffer(time);
     gl.clear(gl.COLOR_BUFFER_BIT);
-    gl.clearColor(0.0, 0.0, 1.0, 1.0);
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
     gl.useProgram(shaderId);
+
 
     gl.uniform1f(uniform_time, time);
     gl.uniform2f(uniform_mpos, cursorX - 669.0, cursorY - 550.0);
 
     mat4.identity(modelViewPersp);
     mat4.perspective(45, canvasx / canvasy, 1, 300, modelViewPersp );
-    mat4.translate(modelViewPersp, [0, 0, -50]);
+    mat4.translate(modelViewPersp, [0, 0, -30]);
     //mat4.ortho( -200, 200, -150, 150, -1, 1, modelViewPersp);
     //mat4.scale(modelViewPersp, [Math.abs(Math.sin(time * 0.0004)) + Math.sin(time * 0.001) + 1.0,  + Math.sin(time * 0.001) + 1.0 , + Math.sin(time * 0.001) + 1.0]);
-    mat4.rotate(modelViewPersp, time * 0.0001, [1, 1, 1]);
+    mat4.rotate(modelViewPersp, 0.19, [1, 0, 0]);
+    mat4.rotate(modelViewPersp, time * 0.001, [0, 1, 0]);
 
 
     gl.uniformMatrix4fv(uniform_matrixProj, false, modelViewPersp);
@@ -278,6 +405,8 @@ function drawScene()
     canvasx = canvas.width;
     canvasy = canvas.height;
 
+    gl.enable(gl.DEPTH);
+
     gl.viewport(0, 0, canvas.width, canvas.height);
 
     pMatrix = mat4.create();
@@ -310,11 +439,13 @@ function drawScene()
     gl.attachShader(shaderId, vShader);
     gl.attachShader(shaderId, fShader);
     gl.linkProgram(shaderId);
+    printShaderStatus(fShader);
 
 
     gl.useProgram(shaderId);
     gl.program = shaderId;
 
+    colorLocation = gl.getUniformLocation(shaderId, "col");
     uniform_time = gl.getUniformLocation(shaderId, "time");
     attribute_vPos = gl.getAttribLocation(shaderId, "vPos");
     uniform_mpos = gl.getUniformLocation(shaderId, "mPos");
@@ -324,8 +455,12 @@ function drawScene()
     //buildGridBuffer();
     vBuffer = gl.createBuffer();
     vBufferTriangle = gl.createBuffer();
+    vBufferPyramid = gl.createBuffer();
+    vBufferBox = gl.createBuffer();
     buildTriangleBuffer(time);
     buildPacmanBuffer();
+    buildPyramidBuffer();
+    buildBox();
     window.requestAnimFrame = (function(callback) {
         return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame ||
             function(callback) {
